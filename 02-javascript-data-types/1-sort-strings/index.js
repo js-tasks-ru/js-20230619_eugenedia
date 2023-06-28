575539361e9b462c361034af9cac7660959e43b6
Eugene <eugenedia@gmail.com>
2023-06-28 23:23:38 +0300

    Доработана сортировка строк

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -6,4 +6,39 @@
  */
 export function sortStrings(arr, param = 'asc') {
 
+  const arrCopy = [...arr];
+
+  if (param == 'asc') {
+    return arrCopy.sort(sortStringAscending);
+  }
+  else if (param == 'desc') {
+    return arrCopy.sort(sortStringDescending);
+  }
+  return arrCopy;
 }
+
+
+function sortStringAscending(a, b) {
+  if (a.localeCompare(b, 'ru', {caseFirst: 'upper'}) < 0) {
+    return -1;
+  }
+  else if (a.localeCompare(b, 'ru', {caseFirst: 'upper'}) > 0) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+function sortStringDescending(a, b) {
+  if (a.localeCompare(b, 'ru', {caseFirst: 'upper'}) < 0) {
+    return 1;
+  }
+  else if (a.localeCompare(b, 'ru', {caseFirst: 'upper'}) > 0) {
+    return -1;
+  }
+  else {
+    return 0;
+  }
+}
+
