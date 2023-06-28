94acb400bbb2d9daef02620b5bce4e00102ff4f8
Eugene <eugenedia@gmail.com>
2023-06-29 01:02:57 +0300

    Исправление по задаче 1-sort-strings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -11,34 +11,18 @@ export function sortStrings(arr, param = 'asc') {
   if (param == 'asc') {
     return arrCopy.sort(sortStringAscending);
   }
-  else if (param == 'desc') {
+  else {
     return arrCopy.sort(sortStringDescending);
   }
-  return arrCopy;
 }
 
 
 function sortStringAscending(a, b) {
-  if (a.localeCompare(b, 'ru', {caseFirst: 'upper'}) < 0) {
-    return -1;
-  }
-  else if (a.localeCompare(b, 'ru', {caseFirst: 'upper'}) > 0) {
-    return 1;
-  }
-  else {
-    return 0;
-  }
+  return a.localeCompare(b, 'ru', {caseFirst: 'upper'});  
 }
+  
 
 function sortStringDescending(a, b) {
-  if (a.localeCompare(b, 'ru', {caseFirst: 'upper'}) < 0) {
-    return 1;
-  }
-  else if (a.localeCompare(b, 'ru', {caseFirst: 'upper'}) > 0) {
-    return -1;
-  }
-  else {
-    return 0;
-  }
+  return -a.localeCompare(b, 'ru', {caseFirst: 'upper'}); 
 }
 
