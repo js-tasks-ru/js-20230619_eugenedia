92304667eac1a51f19d2dca863ee45593c926f6a
Eugene <eugenedia@gmail.com>
2023-07-05 01:25:12 +0300

    Исправление замечаний по др

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,88 +1,132 @@
+const chartHeightMax = 50;
+
 export default class ColumnChart {
-  chartHeight = 50
+  chartHeight = chartHeightMax
 
-  constructor({label = '', link = '', value = '', formatHeading = '', data = []} = {}) {
-    this.element = document.createElement('div');
+  constructor(object = {}) {
 
-    const p = document.createElement('p');
-    p.classList.add('column-chart__title');
-    p.innerHTML = label;
-
-    this.element.append(p);
-
-    const anchor = document.createElement('a');
-
-    anchor.title = 'This is a title';
-    anchor.href = link;
-
-    const linkText = document.createTextNode('link');
-    anchor.append(linkText);
-    anchor.classList.add('column-chart__link');
-
-    this.element.append(anchor);
-
-    const totalValue = document.createElement('span');
-    totalValue.textContent = value;
-    totalValue.classList.add('column-chart__header');
+    const {label = '', link = '', value = '', formatHeading = '', data = []} = object;
     
-    if (formatHeading instanceof Function) {
-      totalValue.textContent = formatHeading(value);
-    }
+    this.label = label;
+    this.link = link;
+    this.value = value;
+    this.formatHeading = formatHeading;
+    this.data = data;
 
-    this.element.append(totalValue);
+    this.render();
+  }
 
-    const columns = this.getColumns(data);
+  render() {
+    const rootElement = this.createRootElement();
 
-    this.element.append(columns);
+    const titleElement = this.createTitleElement(this.label);
 
-    if (!data.length) {
+    const titleAnchorElement = this.createTitleAnchorElement(this.link);
+
+    const createTotalValueElement = this.createTotalValueElement(this.value, this.formatHeading);
+
+    const columnsElements = this.createColumnsElements(this.data);
+
+
+    this.element.append(titleElement);
+    
+    this.element.append(titleAnchorElement);
+
+    this.element.append(createTotalValueElement);
+    
+    this.element.append(columnsElements);
+
+    if (!this.data.length) {
       this.element.classList.add('column-chart_loading');
     }
-
   }
 
   update(newData) {
     const chartBody = this.element.querySelector('.column-chart__chart');
    
-    const columns = this.getColumns(newData);
+    const columnsElements = this.createColumnsElements(newData);
 
-    chartBody.replaceWith(columns);
+    chartBody.replaceWith(columnsElements);
   }
 
   remove() {
-    this.element.remove();
+    if (this.element) {
+      this.element.remove();
+    }
   }
   
   destroy() {
-
+    this.remove();
+    this.element = null;
   }
 
-  getColumns(data) {
-    const columns = document.createElement('div');
-    columns.classList.add('column-chart__chart');
+  createRootElement () {
+    this.element = document.createElement('div');
+    this.element.classList.add('dashboard__chart_label}');
+
+    const mainDivElement = document.createElement('div');
+    mainDivElement.classList.add('column-chart');
+    mainDivElement.style = `--chart-height:${this.chartHeight}`;
+    this.element.append(mainDivElement);
+
+    return this.element;
+  }
+
+  createTitleElement () {
+    const titleElement = document.createElement('div');
+    titleElement.classList.add('column-chart__title');
+    titleElement.innerHTML = this.label;
+    return titleElement;
+  }
+
+  createTitleAnchorElement () {
+    const titleAnchorElement = document.createElement('a');
+    titleAnchorElement.title = 'This is a title';
+    titleAnchorElement.href = this.link;
+
+    const linkTextElement = document.createTextNode('link');
+    titleAnchorElement.append(linkTextElement);
+    titleAnchorElement.classList.add('column-chart__link');
+
+    return titleAnchorElement;
+  }
+
+  createTotalValueElement (value, formatHeading) {
+    const totalValueElement = document.createElement('div');
+    totalValueElement.textContent = this.value;
+    totalValueElement.classList.add('column-chart__header');
+    
+    if (this.formatHeading instanceof Function) {
+      totalValueElement.textContent = formatHeading(value);
+    }
+
+    return totalValueElement;
+  }
+
+  createColumnsElements(data) {
+    const columnsElements = document.createElement('div');
+    columnsElements.classList.add('column-chart__chart');
 
     const columnProps = this.getColumnProps(data);
 
     for (let columnData of columnProps) {
-      const column = document.createElement('div');
-      column.style = `--value: ${columnData.value}`;
-      column.dataset.tooltip = columnData.percent;
-      columns.append(column);
+      const columnElement = document.createElement('div');
+      columnElement.style = `--value: ${columnData.value}`;
+      columnElement.dataset.tooltip = columnData.percent;
+      columnsElements.append(columnElement);
     }
 
-    return columns;
+    return columnsElements;
   }
 
   getColumnProps = function(data) {
     const maxValue = Math.max(...data);
-    const scale = 50 / maxValue;
+    const scale = chartHeightMax / maxValue;
   
-    return data.map(item => {
-      return {
-        percent: (item / maxValue * 100).toFixed(0) + '%',
-        value: String(Math.floor(item * scale))
-      };
-    });
+    return data.map(item => ({
+      percent: (item / maxValue * 100).toFixed(0) + '%',
+      value: String(Math.floor(item * scale))
+    }));
   }
 
   
